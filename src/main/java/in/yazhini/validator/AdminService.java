3e7a3d0618880be51ff535ebda4354dfeb479d33
yazhini <jkyazhini2000@gmail.com>
2021-05-11 17:20:04 +0530

    Renamed AdminService

--- src/main/java/in/yazhini/validator/AdminService.java
+++ src/main/java/in/yazhini/validator/AdminService.java
@@ -3,7 +3,9 @@ package in.yazhini.validator;
 public class AdminService {
 
 	// validate the data in Adminuser
-
+      private AdminService () {
+	
+        }
 	public static boolean login(String userName, String password) {
 		return (userName.trim().equals("Yazhini") && password.trim().equals("Admin123"));
 
