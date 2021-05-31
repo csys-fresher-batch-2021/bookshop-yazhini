1ddca23e2e5ab522fae6cead08e4dccd16c907bc
yazhini <jkyazhini2000@gmail.com>
2021-05-31 15:13:17 +0530

    added changes

--- src/main/java/in/yazhini/util/ConnectionUtil.java
+++ src/main/java/in/yazhini/util/ConnectionUtil.java
@@ -13,15 +13,15 @@ public class ConnectionUtil {
 	}
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		String driverClass = "org.postgresql.Driver";
-		String url = "jdbc:postgresql://localhost:5432/BookApp_db";
-		String username = "postgres";
-		String password = "oracle";
+		String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+		String URL = "jdbc:postgresql://localhost:5432/BookApp_db";
+		String USERNAME = "postgres";
+		String PASSWORD = "oracle";
 		// Step 1: Load the jdbc driver in memory
-		Class.forName(driverClass);
+		Class.forName(DRIVER_CLASS_NAME);
 		// Step 2: Get the connection
-		Connection connection = DriverManager.getConnection(url, username, password);
-        return connection;
+		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		return connection;
 	}
 
 	public static void close1(Connection con, PreparedStatement pst) {
@@ -50,7 +50,7 @@ public class ConnectionUtil {
 			if (con != null) {
 				con.close();
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
