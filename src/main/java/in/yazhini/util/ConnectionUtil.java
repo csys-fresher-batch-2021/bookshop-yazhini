9879a9d05623b8281144c2b89bb4a1fb9c0e2509
yazhini <jkyazhini2000@gmail.com>
2021-05-31 14:48:19 +0530

    added changes

--- src/main/java/in/yazhini/util/ConnectionUtil.java
+++ src/main/java/in/yazhini/util/ConnectionUtil.java
@@ -2,14 +2,10 @@ package in.yazhini.util;
 
 import java.sql.Connection;
 
-
-
-
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 
 public class ConnectionUtil {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
@@ -21,30 +17,31 @@ public class ConnectionUtil {
 		Class.forName(driverClass);
 		// Step 2: Get the connection
 		Connection connection = DriverManager.getConnection(url, username, password);
-		//System.out.println("Connection Created");
+
 		return connection;
 	}
+
 	public static void close1(Connection con, PreparedStatement pst) {
 		try {
 			if (con != null) {
 				con.close();
-				//System.out.println("Connection Released");
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	public static void close2(ResultSet rs,Connection con, PreparedStatement pst) {
+
+	public static void close2(ResultSet rs, Connection con, PreparedStatement pst) {
 		try {
 			if (con != null) {
 				con.close();
-				//System.out.println("Connection Released");
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	}
-	
+}
