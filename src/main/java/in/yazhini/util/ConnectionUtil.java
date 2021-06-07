8b7e9cf4d8e06a6ad2e673a751b41845a8d73918
yazhini <jkyazhini2000@gmail.com>
2021-06-07 19:34:04 +0530

    SQL Changes

--- src/main/java/in/yazhini/util/ConnectionUtil.java
+++ src/main/java/in/yazhini/util/ConnectionUtil.java
@@ -17,12 +17,19 @@ public class ConnectionUtil {
 	private static final String USERNAME = System.getenv("spring.datasource.username");
 	private static final String PASSWORD = System.getenv("spring.datasource.password");
 
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-
-		// Step 1: Load the jdbc driver in memory
-		Class.forName(DRIVER_CLASS_NAME);
+	public static Connection getConnection() throws SQLException {
 		// Step 2: Get the connection
-		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		Connection connection = null;
+		try {
+			// Step 1: Load the jdbc driver in memory
+			Class.forName(DRIVER_CLASS_NAME);
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new SQLException("INVALID USER ID");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return connection;
 	}
 
--- src/main/java/in/yazhini/util/RegistrationDetailsUtil.java
+++ src/main/java/in/yazhini/util/RegistrationDetailsUtil.java
@@ -16,11 +16,18 @@ public class RegistrationDetailsUtil {
 	private static final String PASSWORD = System.getenv("spring.datasource.password");
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-
-		// Step 1: Load the jdbc driver in memory
-		Class.forName(DRIVER_CLASS_NAME);
 		// Step 2: Get the connection
-		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		Connection connection = null;
+		try {
+			// Step 1: Load the jdbc driver in memory
+			Class.forName(DRIVER_CLASS_NAME);
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new SQLException("INVALID USER ID");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return connection;
 	}
 
