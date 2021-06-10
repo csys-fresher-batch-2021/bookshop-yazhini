a6cf0d740f308162f3a147733f8c45194aaf1bf9
yazhini <jkyazhini2000@gmail.com>
2021-06-10 15:04:20 +0530

    Add dao

--- src/test/java/in/yazhini/TestBooks.java
+++ src/test/java/in/yazhini/TestBooks.java
@@ -2,18 +2,20 @@ package in.yazhini;
 
 import static org.junit.Assert.*;
 
+
 import java.util.List;
 
 import org.junit.Test;
 
+import in.yazhini.dao.BookDao;
 import in.yazhini.model.BookDetails;
-import in.yazhini.service.TestBookDetails;
+
 
 public class TestBooks {
 
 	@Test
 	public void numberOfBookTypesTest() {
-		List<BookDetails> bookList = TestBookDetails.getBookList();
+		List<BookDetails> bookList = BookDao.getBookList();
 		assertEquals(4, bookList.size());
 	}
 
