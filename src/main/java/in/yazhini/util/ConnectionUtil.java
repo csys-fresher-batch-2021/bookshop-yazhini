9b19047875f1727b7c44ea205e2808c4c898e9b0
yazhini <jkyazhini2000@gmail.com>
2021-06-25 12:05:50 +0530

    changes

--- src/main/java/in/yazhini/util/ConnectionUtil.java
+++ src/main/java/in/yazhini/util/ConnectionUtil.java
@@ -10,14 +10,15 @@ public class ConnectionUtil {
 	private ConnectionUtil() {
 	}
 
-	private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
-	private static final String URL = "jdbc:postgresql://localhost:5432/online bookshop_db";
-	private static final String USERNAME = "postgres";
-	private static final String PASSWORD = "oracle";
+	private static final String DRIVER_CLASS_NAME = System.getenv("spring.datasource.driver-class-name");
+	private static final String URL = "jdbc:postgresql://projecttracker.ck1ayq0lncmp.ap-south-1.rds.amazonaws.com/bankapp_db";
+	private static final String USERNAME = System.getenv("spring.datasource.username");
+	private static final String PASSWORD = System.getenv("spring.datasource.password");
 
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		Connection connection = null;
 		Class.forName(DRIVER_CLASS_NAME);
-		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		return connection;
 	}
 
