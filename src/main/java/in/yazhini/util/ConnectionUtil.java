17fbb68e18a040de42b4774d0db2f5efda86cbc8
jkyazhini <42486225+jkyazhini@users.noreply.github.com>
2021-06-28 09:00:05 +0530

    Update ConnectionUtil.java

--- src/main/java/in/yazhini/util/ConnectionUtil.java
+++ src/main/java/in/yazhini/util/ConnectionUtil.java
@@ -13,7 +13,7 @@ public class ConnectionUtil {
 	}
 
 	private static final String DRIVER_CLASS_NAME = System.getenv("spring.datasource.driver-class-name");
-	private static final String URL = "jdbc:postgresql://projecttracker.ck1ayq0lncmp.ap-south-1.rds.amazonaws.com/bankapp_db";
+	private static final String URL = "jdbc:postgresql://projecttracker.ck1ayq0lncmp.ap-south-1.rds.amazonaws.com/bookapp_db";
 	private static final String USERNAME = System.getenv("spring.datasource.username");
 	private static final String PASSWORD = System.getenv("spring.datasource.password");
 
